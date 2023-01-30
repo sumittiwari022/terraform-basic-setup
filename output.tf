4e4988f33407969e8c0dab71cf152599b8590135
Rahul Jaiswal <rahul.jaiswal@tothenew.com>
2023-01-30 21:41:42 +0530

    updated description of output resources

--- output.tf
+++ output.tf
@@ -1,12 +1,17 @@
 output "vpc_id" {
 value = module.network.vpc_id
+description = "The id of the created vpc"
 }
 output "secure_db_subnet" {
  value = module.network.secure_subnets[*].tags["Name"]
+ description = "Value of name tags of secure db subnets"
 }
 output "private_subnet" {
  value = module.network.private_subnets[*].tags["Name"]
+ description = "Value of name tags of private subnets"
 }
 output "public_subnet" {
  value = module.network.public_subnets[*].tags["Name"]
+ description = "Value of name tags of public subnets"
 }
+
