ac53fc9fa2acac8fbe934c791eb2d9f9bf6cdf4a
root <rahul.jaiswal@tothenew.com>
2023-02-14 08:54:26 +0000

    pinned tf agent source

--- tf-server.tf
+++ tf-server.tf
@@ -1,5 +1,5 @@
 module "terraform_ec2_agent" {
-  source              = "git::https://github.com/tothenew/terraform-aws-agent.git"
+  source              = "git::https://github.com/tothenew/terraform-aws-agent.git?ref=v0.0.1"
   vpc_id              = data.aws_vpc.selected.id
   subnet_id           = data.aws_subnets.public.ids[0]
 }
