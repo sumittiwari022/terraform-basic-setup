c2059b58ca03d2efa532f6ad45dbe3d4a682d2ab
root <rahul.jaiswal@tothenew.com>
2023-02-21 17:01:27 +0000

    updates: output, iam roles

--- tf-server.tf
+++ tf-server.tf
@@ -6,7 +6,7 @@ module "terraform_ec2_agent" {
   source              = "git::https://github.com/tothenew/terraform-aws-agent.git?ref=v0.1.0"
   vpc_id              = data.aws_vpc.selected.id
   subnet_id           = data.aws_subnets.public.ids[0]
-  iam_instance_profile = module.iam_roles.iam_instance_profile_admin_name
+  iam_instance_profile = module.iam_roles.iam_instance_profile_terraform_tf_agent_name
 
 }
 
