d5b1d9174d3d711757d83f0a51dac68cd28261bf
root <rahul.jaiswal@tothenew.com>
2023-02-21 11:25:47 +0000

    updated instance profile name

--- tf-server.tf
+++ tf-server.tf
@@ -6,6 +6,7 @@ module "terraform_ec2_agent" {
   source              = "git::https://github.com/tothenew/terraform-aws-agent.git"
   vpc_id              = data.aws_vpc.selected.id
   subnet_id           = data.aws_subnets.public.ids[0]
-  iam_instance_profile = module.iam_roles.iam_instance_profile_admin_arn
+  iam_instance_profile = module.iam_roles.iam_instance_profile_admin_name
+
 }
 
