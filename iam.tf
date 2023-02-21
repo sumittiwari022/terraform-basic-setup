3a684628ffe02300f113998b282fffe3519d9b0a
root <rahul.jaiswal@tothenew.com>
2023-02-21 13:14:56 +0000

    add tags for source modules

--- iam.tf
+++ iam.tf
@@ -1,3 +1,3 @@
 module "iam_roles" {
-    source     = "git::https://github.com/tothenew/terraform-aws-identity.git"
+    source     = "git::https://github.com/tothenew/terraform-aws-identity.git?ref=v0.1.0"
 }
