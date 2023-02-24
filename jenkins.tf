35bc8b3c4b10bebd481210c2d619f3d0fe9958cc
rahulttn <rahul.jaiswal@tothenew.com>
2023-02-24 11:50:35 +0000

    updated variable referece

--- jenkins.tf
+++ jenkins.tf
@@ -3,6 +3,6 @@ module "ec2-jenkins" {
   vpc_id               = data.aws_vpc.selected.id
   subnet_ids           = ["${data.aws_subnets.public.ids[0]}"]
   iam_instance_profile = module.iam_roles.iam_instance_profile_admin_name
-  project_name_prefix = "{local.workspace.account_name}-${local.workspace.aws.region}-${local.workspace.project_name}"
+  project_name_prefix = "${local.workspace.account_name}-${local.workspace.aws.region}-${local.workspace.project_name}"
 
 }
--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 module "network" {
   source      = "git::https://github.com/tothenew/terraform-aws-vpc.git?ref=v0.2.0"
   cidr_block  = "10.1.0.0/16"
-  name	      =  "{local.workspace.account_name}-${local.workspace.aws.region}-${local.workspace.project_name}"
+  name	      =  "${local.workspace.account_name}-${local.workspace.aws.region}-${local.workspace.project_name}"
   subnet_bits = 8
 }
 
