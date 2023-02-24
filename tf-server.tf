37a9fadeaacb7c13a559750456a0da6555f56a47
rahulttn <rahul.jaiswal@tothenew.com>
2023-02-24 16:51:29 +0000

    updated tag for tfserver

--- tf-server.tf
+++ tf-server.tf
@@ -3,7 +3,7 @@ module "terraform_ec2_agent" {
      module.network,
      module.iam_roles
     ]
-  source              = "git::https://github.com/tothenew/terraform-aws-agent.git?ref=v0.1.0"
+  source              = "git::https://github.com/tothenew/terraform-aws-agent.git?ref=v0.1.1"
   vpc_id              = data.aws_vpc.selected.id
   subnet_id           = data.aws_subnets.public.ids[0]
   iam_instance_profile = module.iam_roles.iam_instance_profile_terraform_tf_agent_name
