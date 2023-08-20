b13f02d983e1744a1e595bbd7dd3e287b29b8d53
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-20 12:42:16 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source      = "git::https://github.com/tothenew/terraform-aws-vpc.git?ref=v0.2.0"
+  source      = "git::https://github.com/sumittiwari022/terraform-aws-vpc.git?ref=v0.2.0"
   cidr_block  = "10.1.0.0/16"
   name	      =  "${local.workspace.account_name}-${local.workspace.aws.region}-${local.workspace.project_name}"
   subnet_bits = 8
