32d8e5f708ec296ccb847c3179c56486b6506795
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-21 11:56:39 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
 module "iam_roles" {
-    source     = "git::https://github.com/sumittiwari022/terraform-aws-identity.git?ref=v0.1.2"
+    source     = "git::https://github.com/sumittiwari022/terraform-aws-identity.git"
     project_name_prefix = "${local.workspace.account_name}-${local.workspace.aws.region}-${local.workspace.project_name}"
 }
